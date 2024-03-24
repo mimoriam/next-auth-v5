5c21ba47a3402ea9ff349a4d9b448d7ae962495a
Mimo <mimoriam420@gmail.com>
2024-03-25 02:12:22 +0500

    Commit #6 - Login Card setup

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Header } from "@/components/auth/header";
+import { BackButton } from "@/components/auth/back-button";
+import { Social } from "@/components/auth/socials";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton label={backButtonLabel} href={backButtonHref} />
+      </CardFooter>
+    </Card>
+  );
+};
---
+++ components/auth/login-form.tsx
@@ -0,0 +1,16 @@
+import { CardWrapper } from "@/components/auth/card-wrapper";
+
+export const LoginForm = () => {
+  return (
+    <div>
+      <CardWrapper
+        headerLabel="Welcome Back!"
+        backButtonLabel="Don't have an account?"
+        backButtonHref="/auth/register"
+        showSocial
+      >
+        Login Form!
+      </CardWrapper>
+    </div>
+  );
+};
