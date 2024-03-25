070fb6e9737097b9470dda5c55e25e1fce3d1031
Mimo <mimoriam420@gmail.com>
2024-03-26 04:38:41 +0500

    Commit #20 - Some simple sign in checks

--- auth.ts
+++ auth.ts
@@ -14,6 +14,12 @@ export const {
   callbacks: {
     async signIn({ user, account }) {
       if (account?.provider !== "credentials") return true;
+
+      const existingUser = await getUserById(user.id as string);
+
+      // Prevent sign in without email verification
+      if (!existingUser?.emailVerified) return false;
+
       return true;
     },
 
