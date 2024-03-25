8d18ba204a27fbb83620dd33fd908b8d400b8164
Mimo <mimoriam420@gmail.com>
2024-03-26 04:34:26 +0500

    Commit #19 - Added additional field (role) to jwt as well as user session

--- auth.ts
+++ auth.ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
 import prisma from "@/lib/db";
+import { getUserById } from "@/db_functions/user";
+import { UserRole } from "@prisma/client";
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +11,40 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider !== "credentials") return true;
+      return true;
+    },
+
+    async session({ token, session }) {
+      // Access to ID of the current logged in user:
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      // Add new fields from JWT to Session:
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+
+    async jwt({ token }) {
+      // if no token.sub, it means user is logged out
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      // Adding new fields to the JWT:
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
