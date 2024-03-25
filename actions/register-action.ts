a3166ef7620161759b772346914f6f838c92ab18
Mimo <mimoriam420@gmail.com>
2024-03-26 02:55:37 +0500

    Commit #14 - Create user

--- actions/register-action.ts
+++ actions/register-action.ts
@@ -1,7 +1,10 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 import { RegisterSchema } from "@/schemas";
+import prisma from "@/lib/db";
+import { getUserByEmail } from "@/db_functions/user";
 
 export const register_action = async (
   values: z.infer<typeof RegisterSchema>,
@@ -14,5 +17,24 @@ export const register_action = async (
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Verification Email
+
+  return { success: "Confirmation Email sent!" };
 };
