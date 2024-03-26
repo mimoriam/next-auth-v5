e3e1b1260434a78c365ecf940c9c9cf87cf3af3d
Mimo <mimoriam420@gmail.com>
2024-03-27 01:55:29 +0500

    Commit #23.2 - Need to do the same in Next auth callback

--- auth.ts
+++ auth.ts
@@ -25,12 +25,13 @@ export const {
   },
   callbacks: {
     async signIn({ user, account }) {
+      // Allow OAuth without email verification:
       if (account?.provider !== "credentials") return true;
 
       const existingUser = await getUserById(user.id as string);
 
-      // Prevent sign in without email verification
-      // if (!existingUser?.emailVerified) return false;
+      // Prevent sign in without email verification:
+      if (!existingUser?.emailVerified) return false;
 
       return true;
     },
