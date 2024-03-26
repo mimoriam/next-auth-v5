b6459fe6f996b52c0e785e0eab21bb7b50badc50
Mimo <mimoriam420@gmail.com>
2024-03-27 04:37:22 +0500

    Commit #34 - Reverted the hook back. One is for client component and one for server

--- hooks/use-current-user.ts
+++ hooks/use-current-user.ts
@@ -1,7 +1,7 @@
-import { auth } from "@/auth";
+import { useSession } from "next-auth/react";
 
-export const useCurrentUser = async () => {
-  const session = await auth();
+export const useCurrentUser = () => {
+  const session = useSession();
 
-  return session?.user;
+  return session.data?.user;
 };
